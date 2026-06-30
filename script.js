14664ff8effa25c8709e21e20037651db399a5e6
eloahribeirotheicon <ribeiro.eloah@escola.pr.gov.br>
2026-06-29 21:40:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// ================================
+// HydroSense
+// JavaScript do projeto
+// ================================
 
+// Quando a página terminar de carregar
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("HydroSense iniciada com sucesso!");
+
+});
