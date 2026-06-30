933d2ad5f8f1b0e98b6b9fc1f53bfa9e10bb46c3
eloahribeirotheicon <ribeiro.eloah@escola.pr.gov.br>
2026-06-30 00:00:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("HydroSense iniciada com sucesso!");
 
 });
+let agua = 0;
+
+function criarBalao(){
+    const balao = document.createElement("div");
+    balao.classList.add("balao");
+
+    balao.style.left = Math.random() * 90 + "%";
+    balao.style.background = randomColor();
+
+    balao.onclick = function(){
+        balao.remove();
+        agua += 10;
+        document.getElementById("agua").innerText = agua;
+    }
+
+    document.getElementById("baloes").appendChild(balao);
+
+    setTimeout(() => {
+        balao.remove();
+    }, 6000);
+}
+
+function randomColor(){
+    const cores = ["#ff4d4d","#4da6ff","#ffff4d","#ff66cc","#66ff66"];
+    return cores[Math.floor(Math.random() * cores.length)];
+}
+
+setInterval(criarBalao, 800);
