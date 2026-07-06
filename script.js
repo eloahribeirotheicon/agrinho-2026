15ce610406312f1280118c3c45d3e802c71db31e
eloahribeirotheicon <ribeiro.eloah@escola.pr.gov.br>
2026-07-06 19:08:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Função para o Quiz Interativo
+function verificarResposta(acertou) {
+    const resultado = document.getElementById("resultado-quiz");
+    if (acertou) {
+        resultado.innerText = "Resposta Correta! O RaizLink combate exatamente esse desperdício.";
+        resultado.style.color = "green";
+    } else {
+        resultado.innerText = "Tente novamente! A agricultura sem tecnologia desperdiça muita água.";
+        resultado.style.color = "red";
+    }
+}
+
+// Função de Acessibilidade (Aumentar/Diminuir Letra)
+function alternarFonte() {
+    document.body.classList.toggle("fonte-grande");
+}
 
